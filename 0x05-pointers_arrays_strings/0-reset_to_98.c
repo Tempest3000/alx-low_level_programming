f4bda4828722b6a53b26e5c713abda0fb41ad122
Khwezi <gamakhwezi@gmail>
2023-03-27 12:32:25 -0700

    Don't swap horses in crossing a stream

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - a function that takes a pointer to an int a s parameter
+ * reset_to_98n - take a pointer to an int as parameter
  * and updates the value it points to to 98
- * @n: input
+ * @n: point to an int
  * Return: n
  */
 
