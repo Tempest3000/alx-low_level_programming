dee4771b39644336036d7c9703dff71558e812db
Tempest3000 <gamakhwezi@gmail.com>
2023-04-29 03:56:31 -0700

    update

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,20 +3,20 @@
 #include "lists.h"
 
 /**
- * list_len - show the number of elements of a list
- * @h: A linked list
+ * list_len - show the number of elements in a linked list
+ * @h: pointer to the list_t list
  *
- * Return: The number of elements of a list
+ * Return: The number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+	size_t n = 0;
 
 	while (h)
 	{
+		n++;
 		h = h->next;
-		count++;
 	}
 
-	return (count);
+	return (n);
 }
