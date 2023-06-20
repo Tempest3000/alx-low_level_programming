b5fd1ee3f6188d2e7d323e5c7b7ffe10bdedc568
Khwezi Gama <gamakhwezi@gmail.com>
2023-06-20 11:28:20 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog - structure dog
+ * @name: attribute pointer to char name's dog
+ * @owner: attribute pointer to char owner's dog
+ * @age: attribute float age dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int_strlen(char *s);
+
+#endif
