c93157498090a5ff2d785892d9a565d18ca64262
Khwezi Gama <gamakhwezi@gmail.com>
2023-06-27 01:18:02 -0700

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,8 +2,8 @@
 
 /**
  * _pow_recursion - search string for any of a set of bytes
- * @x: base
- * @y: exposant
+ * @x: value to raise
+ * @y: power
  * Return: pointer to byte in 's' that matches one of the bytes in 'accept'
  * or NULL if no such byte is found
  */
@@ -12,9 +12,7 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if (y == 1)
-		return (x);
-	else if (y == 1)
+	if (y == 1)
 		return (1);
 
 	return (x * _pow_recursion(x, y - 1));
